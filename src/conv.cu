efebef116ed290f1ef1505ca1b7aa2cb25f20fc1
Galaxies99 <tony.fang.galaxies@gmail.com>
2021-12-20 08:13:02 +0800

    update format

---
+++ src/conv.cu
@@ -0,0 +1,7 @@
+# include "conv.h"
+
+BasicConvLayer :: BasicConvLayer(int _in_channels, int _out_channels, int _kernel_size, int _size) {
+
+}
+
+BasicConvLayer :: ~BasicConvLayer() {}
---
+++ src/conv.h
@@ -0,0 +1,18 @@
+# include <cuda.h>
+
+# ifndef _LAYER_H_
+# define _LAYER_H_
+# endif
+
+
+// Convolution Layer with stride 1.
+class BasicConvLayer {
+    private:
+        int kernel_size, in_channels, out_channels;
+        int size;
+        float *weight, *bias;
+    
+    public:
+        BasicConvLayer(int _in_channels, int _out_channels, int _kernel_size, int _size);
+        ~ BasicConvLayer();
+}
