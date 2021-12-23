45cfcb0e92444c5c4a46e959157e692a46c5ecef
zhao-hr <49093503+zhao-hr@users.noreply.github.com>
2021-12-23 19:43:24 +0800

    Update fc.h

--- src/fc.h
+++ src/fc.h
@@ -1,7 +1,11 @@
 # ifndef _FC_H_
 # define _FC_H_
+# include <cuda.h>
 # include "cuda_runtime.h"
 # include "utils.h"
+# include <iostream>
+# include <stdio.h>
+using namespace std;
 # endif
 
 
@@ -10,14 +14,17 @@ class FullyConnectedLayer {
     private:
         int in_features, out_features;
         int weight_N, bias_N, output_N;
-        float *weight, *bias, *output;
+        float *weight, *bias;
+        float *h_weight, *h_bias;
     
     public:
         FullyConnectedLayer(int _in_features, int _out_features);
-        void basic_forward(dim3 grid, dim3 block, float *input);
-        void set_params(bool init, float *h_weight, float *h_bias);
+        float* basic_forward(dim3 grid, dim3 block, float *input, const int batch_size);
+        float* cpu_forward(float *input, const int batch_size);
+        void set_params(float *_h_weight = NULL, float *_h_bias = NULL);
         ~ FullyConnectedLayer();
 };
 
-__global__ void fc_basic_weight_forward(float*, float*, float*, const int, const int);
-__global__ void fc_basic_bias_forward(float*, float*, float*, const int);
+void fc_forward_cpu(float *input, float *output, float *weight, float *bias, const int batch_size, const int in_features, const int out_features);
+__global__ void fc_basic_weight_forward(float *input, float *output, float *weight, const int batch_size, const int in_features, const int out_features);
+__global__ void fc_basic_bias_forward(float *input, float *output, float *bias, const int batch_size, const int in_features, const int out_features);
