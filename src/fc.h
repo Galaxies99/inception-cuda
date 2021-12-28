a3a5bd45b05ae3d1560267c3f0e1fbc2e3cdf446
zhao-hr <49093503+zhao-hr@users.noreply.github.com>
2021-12-28 23:08:15 +0800

    optimize fc

--- src/fc.h
+++ src/fc.h
@@ -20,6 +20,7 @@ class FullyConnectedLayer {
     public:
         FullyConnectedLayer(int _in_features, int _out_features);
         double* basic_forward(dim3 grid, dim3 block, double *input, const int batch_size);
+        double* opt_forward(dim3 grid, dim3 block, double *input, const int batch_size);
         double* cpu_forward(double *input, const int batch_size);
         double* cudnn_forward(cudnnHandle_t &handle, double *input, const int batch_size);
         void set_params(double *_h_weight = NULL, double *_h_bias = NULL);
@@ -31,6 +32,8 @@ void fc_forward_cpu(double *input, double *output, double *weight, double *bias,
 __global__ void fc_basic_weight_forward(double *input, double *output, double *weight, const int batch_size, const int in_features, const int out_features);
 
 __global__ void fc_basic_bias_forward(double *input, double *output, double *bias, const int batch_size, const int in_features, const int out_features);
+
+__global__ void fc_opt_weight_forward(double *input, double *output, double *weight, const int batch_size, const int in_features, const int out_features);
 
 void fc_cudnn_forward(cudnnHandle_t& handle, double *input, double *output, double *weight, double *bias, const int batch_size, const int in_channels, const int out_channels);
 
